9cd6b4573ca2480a8a99f89238bba6afaa6f9d1c
gogugogiugogu <o.piotrowski-08@o2.pl>
2025-08-06 17:57:25 +0100

    Add files via upload

---
+++ type&input.py
@@ -0,0 +1,14 @@
+model = "Tesla Model 3"
+cost = 200000
+acceleration = 4.25
+print(type(model))
+print(type(cost))
+print(type(acceleration))
+
+print(model)
+print(cost)
+print(acceleration)
+
+
+name = input()
+print("Hello, ", name)
